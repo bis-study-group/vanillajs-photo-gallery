8fdd622f02db8109db8346b320f60b77c11d9ce2
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-10-09 15:32:23 +0900

    Add solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
-// Insert code here.
+document.querySelectorAll('[data-gallery]').forEach(function(gallery) {
+  const mainImage = gallery.querySelector('[data-gallery-image]');
+  const thumbnails = gallery.querySelectorAll('[data-gallery-src]');
+  const onThumbnailClick = function() {
+    const src = this.getAttribute('data-gallery-src');
+    mainImage.setAttribute('src', src);
+  };
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', onThumbnailClick);
+  });
+});
