3eddf0f95fdcaa0f93834c8bf9e6d18e30ea05f7
shinichi.kurita <shinichi.kurita@ari-jp.com>
2018-10-16 15:30:22 +0900

    課題完了

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
-// Insert code here.
+let thumbnails = document.querySelectorAll('.thumbnail');
+
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', removeActive, false);
+  thumbnail.addEventListener('click', addActive, false);
+});
+
+function removeActive() {
+  let panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
+function addActive() {
+  let thumbnailId = this.getAttribute('id');
+  let panelId = 'panel' + thumbnailId;
+  let panel = document.getElementById(panelId);
+  panel.classList.add('active');
+}
