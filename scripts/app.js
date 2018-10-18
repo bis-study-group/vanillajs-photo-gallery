49817093de3f2737c788c52ca11d244b1f468f72
tenma <logwhitesesame@gmail.com>
2018-10-18 23:55:33 +0900

    画像表示と切り替え機能の実装

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-// Insert code here.
+const [photo1, photo2, photo3] = [
+  document.getElementById('photo1'),
+  document.getElementById('photo2'),
+  document.getElementById('photo3'),
+];
+
+const onClickPhoto = function(choiced) {
+  const target = document.getElementById(choiced);
+  photo1.classList.remove('visible');
+  photo2.classList.remove('visible');
+  photo3.classList.remove('visible');
+  target.classList.add('visible');
+};
