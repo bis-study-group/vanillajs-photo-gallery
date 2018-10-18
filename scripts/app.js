9b1ae51b9276232c8f1003e6ab78e00e1b243311
kana1221 <kanauchi0510@gmail.com>
2018-10-18 22:05:19 +0900

    add slider function

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-// Insert code here.
+const slider = {
+
+  slides: document.querySelectorAll('.slide-item'),
+  thumbnails: document.querySelectorAll('.thumb-item'),
+  isActive: 'is-active',
+  current: null,
+
+  slide: () => {
+    slider.current = slider.slides[0];
+    slider.current.classList.add(slider.isActive);
+
+    for(let i = 0; i < slider.thumbnails.length; i++) {
+      slider.thumbnails[i].addEventListener('click', () => {
+        slider.current.classList.toggle(slider.isActive);
+        slider.current = slider.slides[i];
+        slider.current.classList.toggle(slider.isActive);
+      });
+    }
+  }
+
+}
+
+slider.slide();
