e486f3be0c1ad6b3f49ad66caeb8fe34e41a0614
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-10-19 11:01:38 +0900

    解き直し

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
-document.querySelectorAll('[data-gallery]').forEach(function(gallery) {
-  const mainImage = gallery.querySelector('[data-gallery-image]');
-  const thumbnails = gallery.querySelectorAll('[data-gallery-src]');
-  const onThumbnailClick = function() {
-    const src = this.getAttribute('data-gallery-src');
-    mainImage.setAttribute('src', src);
-  };
-  thumbnails.forEach(function(thumbnail) {
-    thumbnail.addEventListener('click', onThumbnailClick);
-  });
-});
+function initGallery (gallery) {
+  const mainImages = gallery.querySelectorAll('[data-gallery-image]');
+  const thumbnails = gallery.querySelectorAll('[data-gallery-thumbnail]');
+  function onClickThumbnail () {
+    mainImages.forEach(item => item.classList.remove('active'));
+    const code = this.getAttribute('data-gallery-thumbnail');
+    const target = gallery.querySelector(`[data-gallery-image="${code}"]`);
+    target && target.classList.add('active');
+  }
+  thumbnails.forEach(tn => tn.addEventListener('click', onClickThumbnail));
+}
+
+const elem = document.getElementById('myGallery');
+initGallery(elem);
