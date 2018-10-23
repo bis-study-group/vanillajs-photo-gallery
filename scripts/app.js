fd86b7b39eeac8d15270cd9b823d9c42ca92f23a
tenma <logwhitesesame@gmail.com>
2018-10-23 23:04:59 +0900

    スライドを追加_山崎

--- scripts/app.js
+++ scripts/app.js
@@ -2,17 +2,31 @@ let thumbnails = document.querySelectorAll('.thumbnail');
 
 thumbnails.forEach(thumbnail => {
   thumbnail.addEventListener('click', removeActive, false);
+  thumbnail.eventParam = thumbnail.className;
   thumbnail.addEventListener('click', addActive, false);
 });
 
-function removeActive() {
-  let panels = document.querySelectorAll('.panel');
+function removeActive(event) {
+  const check = event.target.eventParam.slice(10);
+  let target;
+  switch (check) {
+    case 'tn1':
+      target = '.panel1';
+      break;
+    case 'tn2':
+      target = '.panel2';
+      break;
+    default:
+      target = null;
+  }
+
+  let panels = document.querySelectorAll(target);
   panels.forEach(panel => {
     panel.classList.remove('active');
   });
 }
 
-function addActive() {
+function addActive(event) {
   let thumbnailId = this.getAttribute('id');
   let panelId = 'panel' + thumbnailId;
   let panel = document.getElementById(panelId);
