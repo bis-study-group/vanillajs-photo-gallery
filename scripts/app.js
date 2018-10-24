ce5d28df8b18e9f252a52ebd6331fde0938fdf98
kana1221 <kanauchi0510@gmail.com>
2018-10-24 19:22:11 +0900

    add slider & fix function

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,35 @@
-function changeimg(url, e) {
-  document.getElementById('img').src = url;
+function changeimg(url, id) {
+  document.getElementById(id).src = url;
 }
+
+// かなうちファンクション
+const changeImage = {
+  mainImage: null,
+  subImages: null,
+  subImagesLength: null,
+
+  init: () => {
+    const slideWrapper = document.querySelector("#slider-container");
+
+    this.mainImage = slideWrapper.querySelector("#slide-main-photo");
+    this.subImages = slideWrapper.querySelectorAll(".thumbnail");
+    this.subImagesLength = this.subImages.length;
+
+    for(let i = 0; i < this.subImagesLength; i++) {
+      this.subImages[i].addEventListener('click', (e) => {
+        const imagePath = changeImage.getImagePath(e)
+        changeImage.setImage(imagePath);
+      });
+    }
+  },
+
+  getImagePath: (event) => {
+    return event.target.getAttribute('src');
+  },
+
+  setImage: (path) => {
+    this.mainImage.setAttribute('src', path);
+  }
+}
+
+changeImage.init();
