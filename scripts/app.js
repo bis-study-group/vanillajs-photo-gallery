f5ca5f5f261736d1ca2ec30572b088aa4e37a0bf
Hiromichi Yamamoto <hiromichi.yamamoto@ari-jp.com>
2018-10-29 21:14:21 +0900

    Vanilla JavaScript で画像ギャラリーを作ろう Part.2

--- scripts/app.js
+++ scripts/app.js
@@ -11,3 +11,36 @@ const onClickPhoto = function(choiced) {
   photo3.classList.remove('visible');
   target.classList.add('visible');
 };
+
+// yamamoto
+changeImage = {
+  mainImages: null,
+  subImages: null,
+  subImagesLength: null,
+
+  getImage: () => {
+    const imageBox = document.querySelector('#image_box');
+
+    this.mainImages = imageBox.querySelectorAll('#main_box img');
+    this.subImages = imageBox.querySelectorAll('.choice');
+    this.subImagesLength = this.subImages.length;
+
+    for (let i = 0; i < this.subImagesLength; i++) {
+      this.subImages[i].addEventListener('click', e => {
+        const clickImageId = changeImage.getClickImageId(e);
+        mainImages.forEach(function(value) {
+          value.classList.remove('visible');
+          if (value.id == clickImageId) {
+            value.classList.add('visible');
+          }
+        });
+      });
+    }
+  },
+  //クリックした画像のIDを取得
+  getClickImageId: event => {
+    return event.target.getAttribute('id');
+  },
+};
+
+changeImage.getImage();
