61aafbd9244b34195a3aa13dc991fda6686bb129
shinichi.kurita <shinichi.kurita@ari-jp.com>
2018-11-01 21:40:11 +0900

    課題完了

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,26 @@
-const slider = {
+const sliders = (slides, thumbnails) => {
+  const slider = {
+    slides: document.querySelectorAll(slides),
+    thumbnails: document.querySelectorAll(thumbnails),
+    isActive: 'is-active',
+    current: null,
 
-  slides: document.querySelectorAll('.slide-item'),
-  thumbnails: document.querySelectorAll('.thumb-item'),
-  isActive: 'is-active',
-  current: null,
+    slide: () => {
+      slider.current = slider.slides[0];
+      slider.current.classList.add(slider.isActive);
 
-  slide: () => {
-    slider.current = slider.slides[0];
-    slider.current.classList.add(slider.isActive);
+      for (let i = 0; i < slider.thumbnails.length; i++) {
+        slider.thumbnails[i].addEventListener('click', () => {
+          slider.current.classList.toggle(slider.isActive);
+          slider.current = slider.slides[i];
+          slider.current.classList.toggle(slider.isActive);
+        });
+      }
+    },
+  };
 
-    for(let i = 0; i < slider.thumbnails.length; i++) {
-      slider.thumbnails[i].addEventListener('click', () => {
-        slider.current.classList.toggle(slider.isActive);
-        slider.current = slider.slides[i];
-        slider.current.classList.toggle(slider.isActive);
-      });
-    }
-  }
+  return slider;
+};
 
-}
-
-slider.slide();
+sliders('.sliders1 .slide-item', '.sliders1 .thumb-item').slide();
+sliders('.sliders2 .slide-item', '.sliders2 .thumb-item').slide();
