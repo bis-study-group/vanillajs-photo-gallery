ae0df49ef911283ff336a3266565cf2d7a3cf994
yuma.kadoi <kadoi82uma@gmail.com>
2019-02-04 20:49:16 +0900

    課題1

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
 // Insert code here.
+document.getElementById('js--click_1').onclick = function() {
+  chage_main_img(1);
+};
+
+document.getElementById('js--click_2').onclick = function() {
+  chage_main_img(2);
+};
+
+document.getElementById('js--click_3').onclick = function() {
+  chage_main_img(3);
+};
+
+chage_main_img = function(num) {
+  var img_name = './images/image_';
+  document.getElementById('main_img').src = img_name + num + '.jpeg';
+};
