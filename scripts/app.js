3cd260c475f63d650f6ad772029e864a201b5632
yuma.kadoi <kadoi82uma@gmail.com>
2019-02-05 23:45:03 +0900

    課題2作成

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,16 @@
 // Insert code here.
-document.getElementById('js--click_1').onclick = function() {
-  chage_main_img(1);
-};
 
-document.getElementById('js--click_2').onclick = function() {
-  chage_main_img(2);
-};
+var img_list = document.getElementsByClassName('img_list_items');
 
-document.getElementById('js--click_3').onclick = function() {
-  chage_main_img(3);
-};
+//各要素にクッリクイベントを定義
+for (let i = 0; i < img_list.length; i++) {
+  img_list[i].addEventListener('click', function() {
+    chage_main_img(img_list[i].dataset.num, img_list[i].dataset.num2);
+  });
+}
 
-chage_main_img = function(num) {
+//画像変更処理
+var chage_main_img = function(num, num2) {
   var img_name = './images/image_';
-  document.getElementById('main_img').src = img_name + num + '.jpeg';
+  document.getElementById('main_img_' + num2).src = img_name + num + '.jpeg';
 };
